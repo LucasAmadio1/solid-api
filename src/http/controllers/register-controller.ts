c1ef962de6e64054295d459ee112565b05157b2d
Lucas Amadio <lucasrochaamadio02@gmail.com>
2026-02-28 15:51:38 -0300

    use case - controller

---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,28 @@
+import { registerUseCase } from '@/use-cases/register-use-case'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+
+export async function registerController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerUseCase({
+      name,
+      email,
+      password,
+    })
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
