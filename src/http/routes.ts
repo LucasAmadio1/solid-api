f1f23a865080e7461d4ced9bd1591f61a568440c
Lucas Amadio <lucasrochaamadio02@gmail.com>
2026-03-05 23:12:17 -0300

    authenticate controller and tests

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register-controller.js'
+import { authenticateController } from './controllers/authenticate-controller.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
+
+  app.post('/sessions', authenticateController)
 }
