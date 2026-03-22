f425450eee13f76a1827a38530c36cc144a3de18
Lucas Amadio <lucasrochaamadio02@gmail.com>
2026-03-22 17:55:52 -0300

    jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register-controller.js'
 import { authenticateController } from './controllers/authenticate-controller.js'
+import { profileController } from './controllers/profile-controller.js'
+import { verifyJWT } from './middlewares/verify-jwt.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
+
+  app.get('/me', { onRequest: [verifyJWT] }, profileController)
 }
